bda9687d1b315486be11382415580281e3854bc2
Pascal T. Hein <pascal.hein@gmx.de>
2019-08-31 10:32:05 +0200

    more commentation, check for existence of installation folder added

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-mkdir /home/$USER/.local/share/redeliste
+# this is where the program is to be installed
+installLoc=/home/$USER/.local/share/redeliste
+
+# create installation folder, if not already existing
+if [! -d $installLoc]; then
+	mkdir /home/$USER/.local/share/redeliste
+fi
+
+# install the files into the folder
 cp $PWD/data/files/* /home/$USER/.local/share/redeliste/
 
+# create desktop starter
 path_desktop="/home/$USER/.local/share/applications/redeliste.desktop"
 
 cp $PWD/data/starter/redeliste.desktop $path_desktop
@@ -10,5 +19,6 @@ cp $PWD/data/starter/redeliste.desktop $path_desktop
 echo "Exec=/home/$USER/.local/share/redeliste/redeliste.py" >> $path_desktop
 echo "Icon=/home/$USER/.local/share/redeliste/redeliste.png" >> $path_desktop
 
+# start up program
 /home/$USER/.local/share/redeliste/redeliste.py
 
